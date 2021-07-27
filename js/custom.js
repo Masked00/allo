2e2203dca838dc2ab797bd38c82b6fb87b1343e7
Potato3inch <54983741+Masked00@users.noreply.github.com>
2021-07-27 20:48:49 +0530

    fix

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,28 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline()
+tl.from(".cl3", { xPercent: -100 })
+    .from(".cl4", { xPercent: 100 })
+    .from(".cl5", { yPercent: -100 })
+    .from(".cl6", { yPercent: 100 });
+
+
+
+ScrollTrigger.create({
+    animation: tl,
+    trigger: "#scroll_section",
+    start: "top top",
+    end: "+=2000",
+    scrub: true,
+    pin: true,
+});
+
+for (i = 0; i <= 3; i++) {
+    console.log(i)
+}
+
+
+
 particlesJS('particles-js',
 
     {
@@ -123,26 +148,4 @@ particlesJS('particles-js',
 
 
 
-gsap.registerPlugin(ScrollTrigger);
-
-const tl = gsap.timeline()
-tl.from(".cl3", { xPercent: -100 })
-    .from(".cl4", { xPercent: 100 })
-    .from(".cl5", { yPercent: -100 })
-    .from(".cl6", { yPercent: 100 });
-
-
-
-ScrollTrigger.create({
-    animation: tl,
-    trigger: "#scroll_section",
-    start: "top top",
-    end: "+=2000",
-    scrub: true,
-    pin: true,
-});
-
-for (i = 0; i <= 3; i++) {
-    console.log(i)
-}
 
